febe5debe4b3430be7b68681841a14574754d714
Kræn Glintborg Bech-Petersen <info@kraenbech.dk>
2019-10-07 17:53:29 +0200

    Added some loops!

--- index.php
+++ index.php
@@ -1,43 +1,24 @@
 <?php get_header(); ?>
 
-  <!-- Hero Section-->
-  <section class="hero">
+
+
+  <?php if (have_posts()) : ?>
+    <section>
     <div class="container">
-      <div class="row">
-        <div class="col-md-12">
-          <h1 class="text-center display-1 heading-black">Hej</h1>
-          <p class="lead mt-5">Proident eu nisi commodo enim deserunt enim duis sunt nostrud anim. Sunt do sit enim veniam nostrud culpa adipisicing do ullamco occaecat et. </p>
-        </div>
 
+
+    <?php while (have_posts()) : the_post(); ?>
+      <div class="post" id="post-<?php the_id(); ?>">
+        <h1><?php the_title(); ?></h1>
+        <p><?php the_content(); ?></p>
       </div>
+
+    <?php endwhile; ?>
     </div>
   </section>
+  <?php else : ?>
+  <?php endif; ?>
 
-  <!-- Portfolio section -->
-
-  <section class="portfolio-section">
-    <div class="container-fluid p-0">
-      <div class="row">
-        <div class="col-lg-6 col-md-6">
-          <a href="#" class="portfolio-item" style="background-image: url('../wp-content/gallery/afernat/afernat_gronlandsk-fugl_web.jpg');"><div class="pi-inner"><h2>Link</h2></div></a>
-        </div>
-        <div class="col-lg-6 col-md-6">
-          <a href="#" class="portfolio-item" style="background-image: url('../wp-content/gallery/tryk/tryk_vldame4.png');"><div class="pi-inner"><h2>Link</h2></div></a>
-        </div>
-        <div class="col-lg-4 col-md-4">
-          <a href="#" class="portfolio-item" style="background-image: url('../wp-content/gallery/skitser/batman5.jpg');"><div class="pi-inner"><h2>Link</h2></div></a>
-        </div>
-        <div class="col-lg-4 col-md-4">
-          <a href="#" class="portfolio-item" style="background-image: url('../wp-content/gallery/filmmagasin-169/16_9_godfather.png');"><div class="pi-inner"><h2>Link</h2></div></a>
-        </div>
-        <div class="col-lg-4 col-md-4">
-          <a href="#" class="portfolio-item" style="background-image: url('../wp-content/gallery/afgang-oese/web_ose_model2.png');"><div class="pi-inner"><h2>Link</h2></div></a>
-        </div>
-        <div class="col-lg-12 col-md-12">
-          <a href="#" class="portfolio-item" style="background-image: url('../wp-content/gallery/skitser/robie_house.jpg');"><div class="pi-inner"><h2>Link</h2></div></a>
-        </div>
-      </div>
-    </div>
-  </section>
+  
 
 <?php get_footer(); ?>
---
+++ page.php
@@ -0,0 +1,20 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) : ?>
+  <section class="post-section">
+  <div class="container">
+
+
+  <?php while (have_posts()) : the_post(); ?>
+    <div class="post" id="post-<?php the_id(); ?>">
+      <h1><?php the_title(); ?></h1>
+      <p><?php the_content(); ?></p>
+    </div>
+
+  <?php endwhile; ?>
+  </div>
+</section>
+<?php else : ?>
+<?php endif; ?>
+
+<?php get_footer(); ?>
